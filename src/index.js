1dcb081d97468ee967ef36a65e4f195f712c311d
Mari Calmer <superlassig@gmail.com>
2023-07-14 12:05:36 +0200

    feat: connect to backend app via apollo on index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import {
 } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3000/graphql'
+  uri: 'https://plant-clinic.fly.dev/graphql'
 });
 
 const authLink = setContext((_, { headers }) => {
