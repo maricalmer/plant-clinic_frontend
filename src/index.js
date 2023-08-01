bf525c4ef6c35a683e69db71787606fb844e8126
Mari Calmer <superlassig@gmail.com>
2023-08-02 00:49:09 +0200

    fix: add credentials to createHttpLink

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ import {
 } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: 'https://peaceful-cliffs-79833-a8770ca52690.herokuapp.com/graphql'
+  uri: 'https://peaceful-cliffs-79833-a8770ca52690.herokuapp.com/graphql',
+  credentials: 'include'
 });
 
 const authLink = setContext((_, { headers }) => {
