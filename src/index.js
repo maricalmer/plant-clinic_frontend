45249e258f382756eed16c03a07392297925415f
Mari Calmer <superlassig@gmail.com>
2023-08-02 12:44:56 +0200

    fix: add more debugger to index.js

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem(AUTH_TOKEN);
+  console.log(token);
   return {
     headers: {
       ...headers,
