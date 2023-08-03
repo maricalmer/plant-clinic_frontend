cb23ff00bfbaf33c0ca4d43e862d82f27852f857
Mari Calmer <superlassig@gmail.com>
2023-08-03 16:43:14 +0200

    fix: remove posts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -47,20 +47,6 @@ const PostList = () => {
     )
   }
 
-  // return (
-  //   <div className="merriweather p-6 min-h-[calc(100vh-4rem)] h-fit">
-  //     <div className="font-bold text-6xl my-14">Recent posts</div>
-  //     <ul className="gap-8 columns-1 sm:columns-2 md:columns-3 lg:columns-4">
-  //       {data && (
-  //         <>
-  //           {data.allPosts.map((post) => (
-  //             <Post key={post.id} post={post} />
-  //           ))}
-  //         </>
-  //       )}
-  //     </ul>
-  //   </div>
-  // );
 };
 
 export default PostList;
