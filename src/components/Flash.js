eaf46a62b4efe12ff48266570005c150298078a3
Mari Calmer <superlassig@gmail.com>
2023-08-03 18:06:02 +0200

    feat: even longer flash

--- src/components/Flash.js
+++ src/components/Flash.js
@@ -2,7 +2,7 @@ import React from 'react';
 import FlashMessage from 'react-flash-message'
 
 const Message = () => (
-  <FlashMessage duration={10000} className="fixed bottom-0">
+  <FlashMessage duration={100000} persistOnHover={true} className="fixed bottom-0">
     <strong>I will disapper in 5 seconds!</strong>
   </FlashMessage>
 )
