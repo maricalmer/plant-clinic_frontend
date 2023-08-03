68f60bfbe8f763c1b55f769b437465c2289ef934
Mari Calmer <superlassig@gmail.com>
2023-08-03 18:07:13 +0200

    feat: move classes on strong element

--- src/components/Flash.js
+++ src/components/Flash.js
@@ -2,8 +2,8 @@ import React from 'react';
 import FlashMessage from 'react-flash-message'
 
 const Message = () => (
-  <FlashMessage duration={100000} persistOnHover={true} className="fixed bottom-0">
-    <strong>I will disapper in 5 seconds!</strong>
+  <FlashMessage duration={100000} persistOnHover={true}>
+    <strong className="fixed bottom-0">I will disapper in 5 seconds!</strong>
   </FlashMessage>
 )
 
