f3eb8368221928b55a940f4b44da91de27c55522
Mari Calmer <superlassig@gmail.com>
2023-08-03 18:08:57 +0200

    feat: wrpper for flash

--- src/components/Flash.js
+++ src/components/Flash.js
@@ -2,9 +2,11 @@ import React from 'react';
 import FlashMessage from 'react-flash-message'
 
 const Message = () => (
-  <FlashMessage duration={100000} persistOnHover={true}>
-    <strong className="fixed bottom-0">I will disapper in 5 seconds!</strong>
-  </FlashMessage>
+  <div className="fixed bottom-0">
+    <FlashMessage duration={100000} persistOnHover={true}>
+      <strong>I will disapper in 5 seconds!</strong>
+    </FlashMessage>
+  </div>
 )
 
 export default Message;
