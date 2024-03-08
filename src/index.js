752ef0e7252d40e98ea3adc7e1af3f4aef18db5a
Mari Calmer <superlassig@gmail.com>
2024-03-08 15:44:31 +0100

    feat: update koyeb backend url

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import {
 
 const httpLink = createHttpLink({
   // uri: 'https://peaceful-cliffs-79833-a8770ca52690.herokuapp.com/graphql'
-  uri: 'https://mixed-barnacle-maricalmer.koyeb.app/graphql'
+  uri: 'https://youthful-caterpillar-maricalmer.koyeb.app/graphql'
 });
 
 const authLink = setContext((_, { headers }) => {
